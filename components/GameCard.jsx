f765443ed0fe2b85c08a858245078945ee6f5c56
DLGuzBuho <109259673+DanielGuzmanV@users.noreply.github.com>
2025-07-20 12:34:56 -0400

    nuevo componente para mostrar la lista

---
+++ components/GameCard.jsx
@@ -0,0 +1,47 @@
+import { StyleSheet, Text, View, Image} from 'react-native';
+
+export function GameCard({dataGame}) {
+  return (
+    <View key={dataGame.slug} style={styles.card}>
+      <Image source={{ uri: dataGame.image }} style={styles.image} />
+
+      <Text style={styles.title}>{dataGame.title}</Text>
+      <Text style={styles.score}>{dataGame.score}</Text>
+      <Text style={styles.description}>{dataGame.description}</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  
+  colorText: {
+    color: 'black'
+  },
+  card: {
+    marginBottom: 50
+  },
+  image: {
+    width: 107,
+    height: 147,
+    borderRadius: 10,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 10,
+    color: 'black',
+  },
+  description: {
+    fontSize: 16,
+    color: 'black',
+  },
+  score: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: 'green',
+    marginTop: 10,
+  }
+
+
+});
+
