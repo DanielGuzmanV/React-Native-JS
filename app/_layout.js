bab5ac24456c7d4bdced161c0ba966524608858d
DLGuzBuho <109259673+DanielGuzmanV@users.noreply.github.com>
2025-08-10 23:09:38 -0400

    configuracion para el safearea

--- app/_layout.js
+++ app/_layout.js
@@ -3,10 +3,8 @@ import { Slot } from "expo-router";
 
 export default function Layout() {
   return (
-    <View>
-      
+    <View className="flex 1 bg-white items-center justify-center">
       <Slot/>
-      
     </View>
   )
 }
