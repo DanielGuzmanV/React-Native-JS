56814d09296267df1f96705c679d60a7f0cfe342
DLGuzBuho <109259673+DanielGuzmanV@users.noreply.github.com>
2025-08-17 14:45:43 -0400

    componente para los iconos

---
+++ components/Icons.jsx
@@ -0,0 +1,9 @@
+import { FontAwesome, FontAwesome6 } from "@expo/vector-icons";
+
+export const CircleInfoIcon = (props) => (
+  <FontAwesome6 name="circle-info" size={32} color="black" {... props}/>
+);
+
+export const HomeIcon =(props) => (
+  <FontAwesome name="home" size={32} color="black" {... props}/>
+)
