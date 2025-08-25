a0769ec66c1429d47bb6c6dc435b6f3a948f7b0e
DLGuzBuho <109259673+DanielGuzmanV@users.noreply.github.com>
2025-08-24 21:51:18 -0400

    se corrigio el flex

--- app/_layout.js
+++ app/_layout.js
@@ -3,7 +3,7 @@ import { Slot } from "expo-router";
 
 export default function Layout() {
   return (
-    <View className="flex 1 bg-white items-center justify-center">
+    <View className="flex-1 bg-white items-center justify-center">
       <Slot/>
     </View>
   )
