dbe56ab8fa061507360946b5c7dacd7752d84286
DLGuzBuho <109259673+DanielGuzmanV@users.noreply.github.com>
2025-09-29 17:52:47 -0400

    se ajusto la dimension del logo

--- components/ReactLogo.jsx
+++ components/ReactLogo.jsx
@@ -3,8 +3,8 @@ import Svg, { G, Rect, Path, Circle } from "react-native-svg"
 export const Logo = (props) => (
   <Svg
     xmlns="http://www.w3.org/2000/svg"
-    width={50}
-    height={50}
+    width={30}
+    height={30}
     viewBox="0 0 73 73"
     {...props}
   >
