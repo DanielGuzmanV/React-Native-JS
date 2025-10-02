f7a0a98ff34fa7020275b658458394f1de1859bd
DLGuzBuho <109259673+DanielGuzmanV@users.noreply.github.com>
2025-10-02 16:32:07 -0400

    se agrego un nuevo icono

--- components/Icons.jsx
+++ components/Icons.jsx
@@ -1,4 +1,5 @@
 import { FontAwesome, FontAwesome6 } from "@expo/vector-icons";
+import AntDesign from '@expo/vector-icons/AntDesign';
 
 export const CircleInfoIcon = (props) => (
   <FontAwesome6 name="circle-info" size={32} color="black" {... props}/>
@@ -10,4 +11,8 @@ export const HomeIcon =(props) => (
 
 export const InfoIcon = (props) => (
   <FontAwesome name="info" size={32} color="white" {...props}/>
+)
+
+export const BasicIcon = (props) => (
+  <AntDesign name="block" size={32} color="white" {...props}/>
 )
